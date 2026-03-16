1a8ce82c7f88c6cd8d6d9b8472e190679ffbec63
usama-official-3 <shusama100121@gmail.com>
2026-03-16 22:00:01 +0500

    MongoDB fix

--- config/drive.js
+++ config/drive.js
@@ -1,16 +1,13 @@
 const { google } = require("googleapis");
 
+// Parse the JSON from the environment variable
+const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON);
+
 const auth = new google.auth.GoogleAuth({
-  credentials: {
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
-  },
+  credentials,
   scopes: ["https://www.googleapis.com/auth/drive"],
 });
 
-const drive = google.drive({
-  version: "v3",
-  auth,
-});
+const drive = google.drive({ version: "v3", auth });
 
 module.exports = drive;
