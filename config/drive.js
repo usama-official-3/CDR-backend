7e557ca7f8ce1118fa3b4a4f1040855a9bd5510e
usama-official-3 <shusama100121@gmail.com>
2026-03-16 21:35:43 +0500

    MongoDB fix

--- config/drive.js
+++ config/drive.js
@@ -3,7 +3,7 @@ const { google } = require("googleapis");
 const auth = new google.auth.GoogleAuth({
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"), // <-- fix newlines
   },
   scopes: ["https://www.googleapis.com/auth/drive"],
 });
